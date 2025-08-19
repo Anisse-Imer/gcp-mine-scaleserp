1b9685fe6c8c6bb30ecb80aecd271ac6453a8d26
Anisse-Imer <anissezou@gmail.com>
2025-08-19 10:38:08 +0200

    Request for Domino - SERP : Part2

--- serp-mine/main.py
+++ serp-mine/main.py
@@ -8,16 +8,23 @@ from dotenv import dotenv_values
 _env = dotenv_values(dotenv_path=Path('.env'))
 API_KEY:str = _env.get("API_KEY_SERP")
 
+key_word:str = "domino’s pizza"
+location:str = "Paris,Paris,Ile-de-France,France"
+device:str = "mobile"
+page:int = 1
+result_page:int = 20
+
+# Doc : https://docs.trajectdata.com/scaleserp/search-api/searches/google/search
 params = {
     "api_key": API_KEY,
-    "q": "keyword here",
-    "location": "98146, Washington, United States",
-    "gl": "us",
-    "hl": "en",
-    "google_domain": "google.com"
+    "q": key_word,
+    "location": location,
+    "page" : page,
+    "num": result_page,
+    "device_type" : device,
+    "google_domain": "google.com",
 }
 
 api_result = requests.get("https://api.scaleserp.com/search", params)
-print(json.dumps(api_result.json()))
-with open("raw-data.json", "w") as outfile:
+with open("resultats.json", "w") as outfile:
     json.dump(api_result.json(), outfile, indent=4)
